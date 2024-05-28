584ee37e129791625fbdd909c39a3aede8ed4c3b
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 22:58:35 +0100

    Revert "improve"
    This reverts commit 68cc396cf69e6b2466c3e81028eff274fc534e0d.

--- Scripts/.temp/binary/ignore/RequiredVersions.ps1
+++ Scripts/.temp/binary/ignore/RequiredVersions.ps1
@@ -1,54 +1,4 @@
-function Install-Module {
-    [CmdletBinding()]
-    param(
-        [string]$ModuleName,
-        [version]$ModuleVersion,
-        [version]$MaximumVersion
-    )
-
-    $modulePath = Join-Path -Path $env:ProgramFiles -ChildPath "WindowsPowerShell\Modules\$ModuleName"
-
-    if (!(Test-Path -Path $modulePath)) {
-        New-Item -ItemType Directory -Path $modulePath -Force | Out-Null
-    }
-
-    $modulePath = Join-Path -Path $modulePath -ChildPath " $($ModuleVersion.Major).$($ModuleVersion.Minor)"
-
-    if (!(Test-Path -Path $modulePath)) {
-        Write-Verbose "Installing module $ModuleName version $ModuleVersion"
-        try {
-            Install-Module -Name $ModuleName -RequiredVersion $ModuleVersion -Force -AllowClobber -Scope CurrentUser
-        } catch {
-            Write-Error "Failed to install module $ModuleName version $ModuleVersion: $_"
-            return $false
-        }
-    } else {
-        $installedModuleVersion = Get-Module -Name $ModuleName -ListAvailable | Where-Object { $_.Version -eq $ModuleVersion }
-
-        if ($installedModuleVersion) {
-            Write-Verbose "Module $ModuleName version $ModuleVersion already installed"
-        } else {
-            Write-Verbose "Updating module $ModuleName to version $ModuleVersion"
-            try {
-                Update-Module -Name $ModuleName -RequiredVersion $ModuleVersion -Force -AllowClobber -Scope CurrentUser
-            } catch {
-                Write-Error "Failed to update module $ModuleName to version $ModuleVersion: $_"
-                return $false
-            }
-        }
-    }
-
-    $maximumModuleVersionPath = Join-Path -Path $modulePath -ChildPath " $($MaximumVersion.Major).$($MaximumVersion.Minor)"
-
-    if (!(Test-Path -Path $maximumModuleVersionPath)) {
-        Write-Verbose "Creating directory for maximum version of module $ModuleName"
-        New-Item -ItemType Directory -Path $maximumModuleVersionPath -Force | Out-Null
-    }
-
-    Write-Verbose "Module $ModuleName installed and configured successfully"
-    return $true
-}
-
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'ModuleList')]
 $ModuleList = @(
     @{
         ModuleName = 'MicrosoftTeams'
@@ -116,21 +66,3 @@ $ModuleList = @(
         MaximumVersion = [version] '0.99.99999'
     }
 )
-
-foreach ($module in $ModuleList) {
-    $moduleName = $module.ModuleName
-    $moduleVersion = $module.ModuleVersion
-    $maximumVersion = $module.MaximumVersion
-
-    if (!(Get-Module -Name $moduleName -ListAvailable)) {
-        Install-Module -ModuleName $moduleName -ModuleVersion $moduleVersion -MaximumVersion $maximumVersion
-    } else {
-        $installedModuleVersion = Get-Module -Name $moduleName -ListAvailable | Select-Object -ExpandProperty Version
-
-        if ($installedModuleVersion -lt $moduleVersion) {
-            Install-Module -ModuleName $moduleName -ModuleVersion $moduleVersion -MaximumVersion $maximumVersion
-        } elseif ($installedModuleVersion -gt $maximumVersion) {
-            Write-Warning "Module $moduleName is already installed at a higher version than the maximum allowed version"
-        }
-    }
-}
