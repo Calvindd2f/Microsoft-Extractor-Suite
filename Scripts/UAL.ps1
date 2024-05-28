8cb2d6895699025a63dea37083c43faba8f4ddb8
Calvindd2f <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 22:33:43 +0100

    improve

--- Scripts/UAL.ps1
+++ Scripts/UAL.ps1
@@ -1,1 +1,10 @@
-. "$PSScriptRoot\Microsoft-Extractor-Suite.psm1";
+# Check if the file exists before loading the module
+if (Test-Path -Path "$PSScriptRoot\Microsoft-Extractor-Suite.psm1") {
+    # Load the module using the fully qualified path
+    Import-Module -Name "$PSScriptRoot\Microsoft-Extractor-Suite.psm1"
+} else {
+    # Handle the error, e.g. by throwing an exception
+    Write-Error "The module 'Microsoft-Extractor-Suite.psm1' could not be found in the path '$PSScriptRoot'."
+    exit 1
+}
+
