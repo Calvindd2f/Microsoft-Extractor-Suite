498b1b3c0645d9922f21ec6ff95d9a99dfb3c5e8
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 23:00:01 +0100

    Revert "improve"
    This reverts commit 8d055ec8d07381033bcb12e0fb698abd8818fd9d.

--- Scripts/Get-ConditionalAccessPolicy.ps1
+++ Scripts/Get-ConditionalAccessPolicy.ps1
@@ -1,6 +1,48 @@
 using module  "$PSScriptRoot\Microsoft-Extractor-Suite.psm1";
 
 Function Get-ConditionalAccessPolicies {
+<#
+    .SYNOPSIS
+    Retrieves all the conditional access policies. 
+
+    .DESCRIPTION
+    Retrieves the risky users from the Entra ID Identity Protection, which marks an account as being at risk based on the pattern of activity for the account.
+    The output will be written to: Output\UserInfo\
+
+    .PARAMETER OutputDir
+    OutputDir is the parameter specifying the output directory.
+    Default: Output\UserInfo
+
+    .PARAMETER Encoding
+    Encoding is the parameter specifying the encoding of the CSV output file.
+    Default: UTF8
+    Aliases: Enc
+
+    .PARAMETER Application
+    Application is the parameter specifying App-only access (access without a user) for authentication and authorization.
+    Default: Delegated access (access on behalf a user)
+    ValidateSet: Application, Delegated
+    ValidateCount: 1
+    ValidateNotNull: $true
+    ValidateNotNullOrEmpty: $true
+    SuppressMessage: 'PS0045', 'Parameter has invalid attributes'
+
+    .EXAMPLE
+    Get-ConditionalAccessPolicies
+    Retrieves all the conditional access policies.
+
+    .EXAMPLE
+    Get-ConditionalAccessPolicies -Application
+    Retrieves all the conditional access policies via application authentication.
+	
+    .EXAMPLE
+    Get-ConditionalAccessPolicies -Encoding utf32
+    Retrieves all the conditional access policies and exports the output to a CSV file with UTF-32 encoding.
+		
+    .EXAMPLE
+    Get-ConditionalAccessPolicies -OutputDir C:\Windows\Temp
+    Retrieves all the conditional access policies and saves the output to the C:\Windows\Temp folder.	
+#>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$false,
@@ -33,16 +75,15 @@ Function Get-ConditionalAccessPolicies {
         [switch]$Application
     )
 
-    # Initialize the results array
+    [System.Management.Automation.OutputType()]
+    [System.Collections.Generic.List`1[System.Management.Automation.PSObject]]
     $Results = @()
 
-    # Set the default encoding if it's empty
-    if (-not $Encoding) {
+    if ($Encoding -eq "" ){
         $Encoding = "UTF8"
     }
 
-    # Connect to Microsoft Graph API
-    if (-not $Application.IsPresent) {
+    if (!($Application.IsPresent)) {
         try {
             $Credential = Get-Credential -Message "Enter your credentials for connecting to Microsoft Graph API" -ErrorAction Stop
             Connect-MgGraph -Credential $Credential -Scopes Policy.Read.All -NoWelcome -ErrorAction Stop
@@ -53,43 +94,54 @@ Function Get-ConditionalAccessPolicies {
     }
 
     try {
-        # Retrieve conditional access policies
-        $policies = get-MgIdentityConditionalAccessPolicy -ErrorAction stop
+        $areYouConnected = get-MgIdentityConditionalAccessPolicy -ErrorAction stop
     }
     catch {
         Write-Error "[Error] Failed to retrieve conditional access policies: $_" -ErrorAction Stop
     }
 
-    # Generate the output file path
-    $Date = Get-Date -Format "yyyyMMddHHmmss"
-    $FilePath = Join-Path -Path $OutputDir -ChildPath "$($Date)-ConditionalAccessPolicy.csv"
+    $Date = [datetime]::Now.ToString('yyyyMMddHHmmss')
+    $FilePath = "$OutputDir\$($Date)-ConditionalAccessPolicy.csv"
 
-    # Process each policy
-    foreach ($policy in $policies) {
-        $myObject = New-Object -TypeName PSObject -Property @{
-            DisplayName                   = $policy.DisplayName
-            CreatedDateTime               = $policy.CreatedDateTime
-            Description                   = $policy.Description
-            Id                            = $policy.Id
-            ModifiedDateTime              = $policy.ModifiedDateTime
-            State                         = $policy.State
-            ClientAppTypes                = $policy.Conditions.ClientAppTypes -join ', '
-            ServicePrincipalRiskLevels    = $policy.Conditions.ServicePrincipalRiskLevels -join ', '
-            SignInRiskLevels              = $policy.Conditions.SignInRiskLevels -join ', '
-            UserRiskLevels                = $policy.Conditions.UserRiskLevels -join ', '
-            BuiltInControls               = $policy.GrantControls.BuiltInControls -join ', '
-            CustomAuthenticationFactors   = $policy.GrantControls.CustomAuthenticationFactors -join ', '
-            ClientOperatorAppTypes        = $policy.GrantControls.Operator -join ', '
-            TermsOfUse                    = $policy.GrantControls.TermsOfUse -join ', '
-            DisableResilienceDefaults     = $policy.SessionControls.DisableResilienceDefaults
+    $Results = @()
+
+    get-MgIdentityConditionalAccessPolicy -all | ForEach-Object {
+        $myObject = [PSCustomObject]@{
+            DisplayName                   = "-"
+            CreatedDateTime               = "-"
+            Description                   = "-"
+            Id                            = "-"
+            ModifiedDateTime              = "-"
+            State                         = "-"
+            ClientAppTypes                = "-"
+            ServicePrincipalRiskLevels    = "-"
+            SignInRiskLevels              = "-"
+            UserRiskLevels                = "-"
+            BuiltInControls               = "-"
+            CustomAuthenticationFactors   = "-"
+            ClientOperatorAppTypes        = "-"
+            TermsOfUse                    = "-"
+            DisableResilienceDefaults     = "-"
         }
 
-        # Add the object to the results array
+        $myobject.DisplayName = $_.DisplayName
+        $myobject.CreatedDateTime = $_.CreatedDateTime
+        $myobject.Description = $_.Description
+        $myobject.Id = $_.Id
+        $myobject.ModifiedDateTime = $_.ModifiedDateTime
+        $myobject.State = $_.State
+        $myobject.ClientAppTypes = $_.Conditions.ClientAppTypes | out-string
+        $myobject.ServicePrincipalRiskLevels = $_.Conditions.ServicePrincipalRiskLevels | out-string
+        $myobject.SignInRiskLevels = $_.Conditions.SignInRiskLevels | out-string
+        $myobject.UserRiskLevels = $_.Conditions.UserRiskLevels | out-string
+        $myobject.BuiltInControls = $_.GrantControls.BuiltInControls | out-string
+        $myobject.CustomAuthenticationFactors = $_.GrantControls.CustomAuthenticationFactors | out-string
+        $myobject.ClientOperatorAppTypes = $_.GrantControls.Operator | out-string
+        $myobject.TermsOfUse = $_.GrantControls.TermsOfUse | out-string
+        $myobject.DisableResilienceDefaults = $_.SessionControls.DisableResilienceDefaults | out-string
         $Results += $myObject
     }
 
-    # Export the results to a CSV file
     $Results | Export-Csv -Path $filePath -NoTypeInformation -Encoding $Encoding
     Write-Host "[INFO] Output written to $filePath" -ForegroundColor Green
 }
-
