c88e3b51870bfb3db0b4787e1d6528ef5ade9412
Calvindd2f <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 22:33:30 +0100

    improve

--- Scripts/Get-MessageTraceLog.ps1
+++ Scripts/Get-MessageTraceLog.ps1
@@ -1,78 +1,37 @@
-using module  "$PSScriptRoot\Microsoft-Extractor-Suite.psm1";
+using module "$PSScriptRoot\Microsoft-Extractor-Suite.psm1"
 
-# This contains a function to collect the Message Trace logging.
-Function StartDateMTL
-{
-    [CmdletBinding()]
-    param()
-
-    if ([string]::IsNullOrEmpty($script:StartDate))
-    {
-        $script:StartDate = [datetime]::UtcNow.AddDays(-10)
-        Write-LogFile -Message "[INFO] No start date provided by user. Setting the start date to: $($script:StartDate.ToString("yyyy-MM-ddTHH:mm:ssK"))" -Color "Yellow"
-    }
-    else
-    {
-        $script:StartDate = Validate-Date $startDate
-        if ($null -eq $script:StartDate) { Write-LogFile -Message "[WARNING] Not A valid start date and time, make sure to use YYYY-MM-DD" -Color "Red"} 
-    }
-}
-
-function EndDateMTL
-{
-    [CmdletBinding()]
-    param()
-
-    if ([string]::IsNullOrEmpty($script:EndDate))
-    {
-        $script:EndDate = [datetime]::UtcNow
-        Write-LogFile -Message "[INFO] No end date provided by user. Setting the end date to: $($script:EndDate.ToString("yyyy-MM-ddTHH:mm:ssK"))" -Color "Yellow"
-    }
-    else
-    {
-        $script:EndDate = Validate-Date $endDate
-        if ($null -eq $script:EndDate) {Write-LogFile -Message "[WARNING] Not A valid end date and time, make sure to use YYYY-MM-DD" -Color "Red"} 
-    }
-}
-
-Function Validate-Date
-{
+# Define the functions
+Function Validate-Date {
     [CmdletBinding()]
     param([Parameter(Mandatory=$true)] [string]$Date)
 
-    if ($Date -match "^\d{4}-\d{2}-\d{2}$")
-    {
+    if ($Date -match "^\d{4}-\d{2}-\d{2}$") {
         return [datetime]::ParseExact($Date, "yyyy-MM-dd", $null)
     }
 
     return $null
 }
 
-Function Validate-UserIds
-{
+Function Validate-UserIds {
     [CmdletBinding()]
     param([Parameter(Mandatory=$true)] [string]$UserIds)
 
-    if ($UserIds -match "^\*@.*")
-    {
+    if ($UserIds -match "^\*@.*") {
         return $UserIds.Replace("*@","")
     }
 
-    if ($UserIds -match "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
-    {
+    if ($UserIds -match "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$") {
         return $UserIds
     }
 
-    if ($UserIds -match "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(,\s*[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})*$")
-    {
+    if ($UserIds -match "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(,\s*[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})*$") {
         return $UserIds.Split(',') | ForEach-Object { $_.Trim() }
     }
 
     return $null
 }
 
-Function Test-Connection
-{
+Function Test-Connection {
     [CmdletBinding()]
     param()
 
@@ -85,8 +44,7 @@ Function Test-Connection
     }
 }
 
-Function Write-LogFile
-{
+Function Write-LogFile {
     [CmdletBinding()]
     param([Parameter(Mandatory=$true)] [string]$Message, [string]$Color = "White")
 
@@ -95,19 +53,52 @@ Function Write-LogFile
     $message | Out-File -FilePath ".\log.txt" -Append
 }
 
-Function Test-Directory
-{
+Function Test-Directory {
     [CmdletBinding()]
     param([Parameter(Mandatory=$true)] [string]$Directory)
 
-    if (-not (Test-Path $Directory))
-    {
+    if (-not (Test-Path $Directory)) {
         New-Item -ItemType Directory -Force -Path $Directory | Out-Null
     }
 }
 
-Function Get-MessageTraceLog
-{
+# Initialize variables
+$StartDate = $null
+$EndDate = $null
+
+Function StartDate =MTL {
+    [CmdletBinding()]
+    param()
+
+    if ($null -eq $StartDate) {
+        $StartDate = [datetime]::UtcNow.AddDays(-10)
+        Write-LogFile -Message "[INFO] No start date provided by user. Setting the start date to: $($StartDate.ToString("yyyy-MM-ddTHH:mm:ssK"))" -Color "Yellow"
+    }
+    else {
+        $StartDate = Validate-Date $StartDate
+        if ($null -eq $StartDate) {
+            Write-LogFile -Message "[WARNING] Not A valid start date and time, make sure to use YYYY-MM-DD" -Color "Red"
+        }
+    }
+}
+
+Function EndDateMTL {
+    [CmdletBinding()]
+    param()
+
+    if ($null -eq $EndDate) {
+        $EndDate = [datetime]::UtcNow
+        Write-LogFile -Message "[INFO] No end date provided by user. Setting the end date to: $($EndDate.ToString("yyyy-MM-ddTHH:mm:ssK"))" -Color "Yellow"
+    }
+    else {
+        $EndDate = Validate-Date $EndDate
+        if ($null -eq $EndDate) {
+            Write-LogFile -Message "[WARNING] Not A valid end date and time, make sure to use YYYY-MM-DD" -Color "Red"
+        }
+    }
+}
+
+Function Get-MessageTraceLog {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$false)]
