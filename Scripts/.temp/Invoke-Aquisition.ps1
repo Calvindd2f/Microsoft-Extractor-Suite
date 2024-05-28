d87296f3f4141627c08a00e87fea50c46621c161
Calvindd2f <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 22:33:04 +0100

    improve

--- Scripts/.temp/Invoke-Aquisition.ps1
+++ Scripts/.temp/Invoke-Aquisition.ps1
@@ -5,32 +5,57 @@ using namespace System.Collections.Generic
 . "$PSScriptRoot\Microsoft-Extractor-Suite.psm1"
 . "$PSScriptRoot\Scripts\*.ps1"
 
-function Invoke-Aquisition {
+function Invoke-Acquisition {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true)]
         [string]$user,
 
         [Parameter(Mandatory=$true)]
-        [string]$OutputDir,
+        [string]$outputDir,
 
         [Parameter(Mandatory=$true)]
-        [string]$Encoding
+        [string]$encoding
     )
-
-    $jobs = @()
 
     # Creating a custom object
     $response = [PSCustomObject]@{
-        Body = "The name passed was [$($global:Name)] with value of [$($global:Value)]"
+        Body = "The user name is $user, output directory is $outputDir and encoding is $encoding."
     }
 
-    # Setting the query parameters as global variables
-    $QueryParams = $Request.Query.GetEnumerator()
-    foreach ($param in $QueryParams) {
-        New-Variable -Name $param.Key -Value $param.Value -Force -Scope Global
+    # No need to set query parameters as global variables
+    # Setting the query parameters as local variables instead
+    $queryParams = $Request.Query.GetEnumerator()
+    foreach ($param in $queryParams) {
+        Write-Verbose "Setting variable $($param.Key) to $($param.Value)"
+        Set-Variable -Name $param.Key -Value $param.Value -Force -Scope Local
     }
+
+    # Starting a job to perform some long-running operation
+    $jobs += Start-Job -ScriptBlock {
+        # Simulating long-running operation
+        Start-Sleep -Seconds 10
+        # Returning the result
+        [PSCustomObject]@{
+            Message = "Long-running operation completed."
+        }
+    }
+
+    # Returning the response object
+    $response
 }
 
 # Calling the function
-Invoke-Aquisition -user "testUser" -OutputDir "C:\temp" -Encoding "UTF8"
+$acquisitionResponse = Invoke-Acquisition -user "testUser" -outputDir "C:\temp" -encoding "UTF8"
+
+# Displaying the response
+Write-Output "Acquisition response: $($acquisitionResponse.Body)"
+
+# Displaying the jobs started
+Write-Output "Jobs started: $($jobs.Count)"
+
+# Getting the results of the jobs
+$jobResults = Receive-Job -Job $jobs
+
+# Displaying the results of the jobs
+Write-Output "Job results: $($jobResults.Message)"
