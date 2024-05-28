433fc180a4429c742dfcd79c5ef3310250f06204
Calvindd2f <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 22:33:06 +0100

    improve

--- Microsoft-Extractor-Suite.psm1
+++ Microsoft-Extractor-Suite.psm1
@@ -1,59 +1,90 @@
 # Set supported TLS methods
+Add-Type -TypeDefinition @"
+    using System.Net;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(ServicePoint sPoint, X509Certificate certificate, WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-#Enable TLS, TLS1.1, TLS1.2, TLS1.3 in this session if they are available
-IF([Net.SecurityProtocolType]::Tls) {[Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls}
-IF([Net.SecurityProtocolType]::Tls11) {[Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls11}
-IF([Net.SecurityProtocolType]::Tls12) {[Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12}
-IF([Net.SecurityProtocolType]::Tls13) {[Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls13}
 
-Function Write-Log([string]$log,[switch]$show){
+# Enable TLS, TLS1.1, TLS1.2, TLS1.3 in this session if they are available
+$tlsProtocols = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Tls13
+foreach ($protocol in $tlsProtocols) {
+    if ($protocol -in [Net.ServicePointManager]::SecurityProtocol) {
+        continue
+    }
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $protocol
+}
+
+function Write-Log([string]$log, [switch]$show) {
     [string]$logtime = $((Get-Date -Format "[dd/MM/yyyy HH:mm:ss zz] |").ToString())
-    foreach($line in $($log -split "`n")){
-        if($VerbosePreference -eq 'Continue' -or $show -eq $true){[console]::WriteLine( "$logtime $line"}
-      if(-not (Test-Path "Log.log")){New-Item -ItemType File -Path "Log.log"}
-      Add-Content -Path "Log.log" -Value "$logtime $line" -ErrorAction Stop
+    foreach ($line in $($log -split "`n")) {
+        if ($VerbosePreference -eq 'Continue' -or $show -eq $true) {
+            [console]::WriteLine("$logtime $line")
+        }
+        if (-not (Test-Path "Log.log")) {
+            New-Item -ItemType File -Path "Log.log"
+        }
+        try {
+            Add-Content -Path "Log.log" -Value "$logtime $line" -ErrorAction Stop
+        } catch {
+            Write-Error "Failed to write to log file: $_"
+        }
     }
 }
 
-$manifest = Import-PowerShellDataFile "$PSScriptRoot\Microsoft-Extractor-Suite.psd1"
+$manifestPath = Join-Path $PSScriptRoot "Microsoft-Extractor-Suite.psd1"
+if (-not (Test-Path $manifestPath)) {
+    Write-Error "PowerShell data file not found: $manifestPath"
+    return
+}
+
+$manifest = Import-PowerShellDataFile $manifestPath
 $version = $manifest.ModuleVersion
 $host.ui.RawUI.WindowTitle = "Microsoft-Extractor-Suite $version"
 
-$logo = @'
- +-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+
- |M|i|c|r|o|s|o|f|t| |E|x|t|r|a|c|t|o|r| |S|u|i|t|e|
- +-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+
-Copyright (c) 2024 Invictus Incident Response
-Created by Joey Rentenaar & Korstiaan Stam
-'@
+$logoPath = Join-Path $PSScriptRoot "logo.txt"
+if (-not (Test-Path $logoPath)) {
+    Write-Error "Logo file not found: $logoPath"
+    return
+}
 
+$logo = Get-Content $logoPath
 [console]::ForegroundColor = 'Yellow'
 [console]::WriteLine("$logo")
 [console]::ForegroundColor = 'White'
 
 $Parameters = @{
-	OutputDir = $global:OutputDir
+    OutputDir = $global:OutputDir
 }
+
+if (-not $Parameters.OutputDir) {
+    $Parameters.OutputDir = Get-Item -Path $PSScriptRoot
+}
+
+if (-not (Test-Path $Parameters.OutputDir)) {
+    Write-Error "Output directory not found: $($Parameters.OutputDir)"
+    return
+}
+
 Assert-GlobalVariables @Parameters
 
 $Global:retryCount = 0
 
-Function StartDate
-{
-    if ([string]::IsNullOrWhiteSpace($startDate))
-    {
+function StartDate() {
+    if ([string]::IsNullOrWhiteSpace($startDate)) {
         $daysToAdd = -90
         $message = "[INFO] No start date provided by user setting the start date to: {0}" -f ([datetime]::Now.ToUniversalTime().AddDays($daysToAdd).ToString('yyyy-MM-ddTHH:mm:ssK'))
         $color = 'Yellow'
-    }
-    else
-    {
+    } else {
         $daysToAdd = -30
         $message = "[INFO] No start date provided by user setting the start date to: {0}" -f ($startDate -as [datetime]).ToString('yyyy-MM-ddTHH:mm:ssK')
         $color = 'Yellow'
     }
-    
+
     $Global:StartDate = [datetime]::Now.ToUniversalTime().AddDays($daysToAdd)
-    
+
     Write-Log -log $message -show
-    return @(($
+    return @(($Global:StartDate)
