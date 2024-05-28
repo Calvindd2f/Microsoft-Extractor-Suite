96735fd95eb568e06a440f6cd6f2943b4fc9c8a5
Calvindd2f <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 08:41:07 +0100

    improve

--- Scripts/Get-Rules.ps1
+++ Scripts/Get-Rules.ps1
@@ -1,232 +1,208 @@
-. "$PSScriptRoot\Microsoft-Extractor-Suite.psm1";
+# Load required modules
+. "$PSScriptRoot\Microsoft-Extractor-Suite.psm1"
 
 # This contains functions to display or collect the inbox and transport rules.
 
+# Gets the current date and time in the format "yyyyMMddHHmm"
 $date = Get-Date -Format "yyyyMMddHHmm"
-function Show-TransportRules
-{
-<#    .SYNOPSIS
-    Shows the transport rules in your organization.
 
-    .DESCRIPTION
-    Shows the transport rules in your organization.
+# Displays the transport rules in the organization
+function Show-TransportRules {
+    <#
+        .SYNOPSIS
+            Displays the transport rules in the organization.
 
-    .Example
-    Show-TransportRules
-#>
-	$transportRules = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/policies/transportRules" -OutputType Microsoft.Open.MSG.Clients.TransportRules.MicrosoftGraph.TrustPolicy
+        .DESCRIPTION
+            Displays the transport rules in the organization.
 
-	if ($null -ne $transportRules) {
-		
-		write-LogFile -Message "[INFO] Checking all TransportRules"
-		$transportRules.Value | ForEach-Object {
-			[void](write-LogFile -Message "[INFO] Found a TransportRule" -Color "Green")
-			[void](write-LogFile -Message "Rule Name $($_.DisplayName)" -Color "Yellow")
-			[void](write-LogFile -Message "Rule CreatedBy: $($_.CreatedBy.Application.DisplayName)" -Color "Yellow")
-			[void](write-LogFile -Message "When Changed: $($_.CreatedBy.DateTime)" -Color "Yellow")
-			[void](write-LogFile -Message "Rule State: $($_.IsEnabled)" -Color "Yellow")
-			[void](write-LogFile -Message "Description: $($_.Description)" -Color "Yellow")
-		}
-	}
+        .EXAMPLE
+            Show-TransportRules
+    #>
+
+    # Gets the transport rules from Microsoft Graph API
+    $transportRules = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/policies/transportRules" -OutputType Microsoft.Open.MSG.Clients.TransportRules.MicrosoftGraph.TrustPolicy
+
+    if ($null -ne $transportRules) {
+        # Displays information message
+        Write-LogFile -Message "[INFO] Checking all TransportRules"
+
+        # Loops through each transport rule
+        $transportRules.Value | ForEach-Object {
+            # Displays a message indicating that a transport rule is found
+            [void](Write-LogFile -Message "[INFO] Found a TransportRule" -Color "Green")
+
+            # Displays the name, created by, when changed, state, and description of the transport rule
+            [void](Write-LogFile -Message "Rule Name: $($_.DisplayName)" -Color "Yellow")
+            [void](Write-LogFile -Message "Rule CreatedBy: $($_.CreatedBy.Application.DisplayName)" -Color "Yellow")
+            [void](Write-LogFile -Message "When Changed: $($_.CreatedBy.DateTime)" -Color "Yellow")
+            [void](Write-LogFile -Message "Rule State: $($_.IsEnabled)" -Color "Yellow")
+            [void](Write-LogFile -Message "Description: $($_.Description)" -Color "Yellow")
+        }
+    }
 }
 
-function Get-TransportRules
-{
-<#    .SYNOPSIS
-    Collects all transport rules in your organization.
+# Collects the transport rules in the organization and saves them to a CSV file
+function Get-TransportRules {
+    <#
+        .SYNOPSIS
+            Collects all transport rules in the organization and saves them to a CSV file.
 
-    .DESCRIPTION
-    Collects all transport rules in your organization.
-	The output will be written to a CSV file called "TransportRules.csv".
+        .DESCRIPTION
+            Collects all transport rules in the organization and saves them to a CSV file.
 
-	.PARAMETER OutputDir
-	OutputDir is the parameter specifying the output directory.
-	Default: Output\Rules
+        .PARAMETER OutputDir
+            OutputDir is the parameter specifying the output directory.
+            Default: Output\Rules
 
-	.PARAMETER Encoding
-	Encoding is the parameter specifying the encoding of the CSV output file.
-	Default: UTF8
+        .PARAMETER Encoding
+            Encoding is the parameter specifying the encoding of the CSV output file.
+            Default: UTF8
 
-    .Example
-    Get-TransportRules
-#>
+        .EXAMPLE
+            Get-TransportRules
+    #>
 
-	[CmdletBinding()]
-	param (
-		[string]$OutputDir,
-		[string]$Encoding
-	)
+    [CmdletBinding()]
+    param (
+        [string]$OutputDir = "Output\Rules",
+        [string]$Encoding = "UTF8"
+    )
 
-	if ($OutputDir -eq ""{
-		$OutputDir = "Output\Rules"
-		if (!(test-path $OutputDir)) {
-			[void](New-Item -ItemType Directory -Force -Name $OutputDir)
-			write-LogFile -Message "[INFO] Creating the following directory: $OutputDir"
-		}
-	}
+    # Validates the output directory
+    if (!(Test-Path -Path $OutputDir)) {
+        # Creates the output directory if it doesn't exist
+        New-Item -ItemType Directory -Force -Path $OutputDir | Out-Null
+        Write-LogFile -Message "[INFO] Creating the following directory: $OutputDir"
+    }
 
-	else{
-		if (Test-Path -Path $OutputDir) {
-  			write-LogFile -Message "[INFO] Custom directory set to: $OutputDir"
-     	}
+    $filename = "$($date)-TransportRules.csv"
+    $outputDirectory = Join-Path $OutputDir $filename
 
-       	else {
-    		write-Error "[Error] Custom directory invalid: $OutputDir exiting script" -ErrorAction Stop
-    		write-LogFile -Message "[Error] Custom directory invalid: $OutputDir exiting script"
-	 	}
-   	}
+    # Gets the transport rules from Microsoft Graph API
+    $transportRules = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/policies/transportRules" -OutputType Microsoft.Open.MSG.Clients.TransportRules.MicrosoftGraph.TrustPolicy
 
-	$filename = "$($date)-TransportRules.csv"
-	$outputDirectory = Join-Path $OutputDir $filename
+    if ($null -ne $transportRules) {
+        # Creates a StreamWriter to write to the CSV file
+        $streamWriter = [IO.StreamWriter]::Create($outputDirectory, $false, [Text.Utf8Encoding]::new($Encoding, $false, $true))
 
-	if ($Encoding -eq ""{
-		$Encoding = "UTF8"
-	}
+        try {
+            # Writes the header row to the CSV file
+            $streamWriter.WriteLine("Name,Description,CreatedBy,WhenChanged,State")
 
-	$transportRules = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/policies/transportRules" -OutputType Microsoft.Open.MSG.Clients.TransportRules.MicrosoftGraph.TrustPolicy
+            # Loops through each transport rule
+            $transportRules.Value | ForEach-Object {
+                # Writes the properties of the transport rule to the CSV file
+                $streamWriter.WriteLine("$($_.DisplayName),$($_.Description),$($_.CreatedBy.Application.DisplayName),$($_.CreatedBy.DateTime),$($_.IsEnabled)")
+            }
+        }
+        finally {
+            # Closes the StreamWriter
+            $streamWriter.Dispose()
+        }
 
-	if ($null -ne $transportRules) {
-		$streamWriter = [IO.StreamWriter]::Create($outputDirectory, $false, [Text.Utf8Encoding]::new($Encoding, $false, $true))
-		try {
-			foreach ($transportRule in $transportRules.Value) {
-				$streamWriter.WriteLine("Name,$($transportRule.DisplayName),Description,$($transportRule.Description),CreatedBy,$($transportRule.CreatedBy.Application.DisplayName),WhenChanged,$($transportRule.CreatedBy.DateTime),State,$($transportRule.IsEnabled)")
-			}
-		}
-		finally {
-			$streamWriter.Dispose()
-		}
-		write-LogFile -Message "[INFO] Transport rules are collected and writen to: $outputDirectory" -Color "Green"
-	}
+        # Displays a success message
+        Write-LogFile -Message "[INFO] Transport rules are collected and writen to: $outputDirectory" -Color "Green"
+    }
 }
 
+# Collects the mailbox rules for the specified users and saves them to a CSV file
+function Get-MailboxRules {
+    <#
+        .SYNOPSIS
+            Collects all the mailbox rules for the specified users and saves them to a CSV file.
 
-function Get-MailboxRules{
-{
-<#
+        .DESCRIPTION
+            Collects all the mailbox rules for the specified users and saves them to a CSV file.
 
-    .SYNOPSIS
-    Collects all the mailbox rules in your organization.
+        .PARAMETER UserIds
+            UserIds is the Identity parameter specifies the Inbox rule that you want to view.
 
-    .DESCRIPTION
-    Collects all the mailbox rules in your organization.
-	The output will be written to a CSV file called "InboxRules.csv".
+        .PARAMETER OutputDir
+            OutputDir is the parameter specifying the output directory.
+            Default: Output\Rules
 
-	.Parameter UserIds
-    UserIds is the Identity parameter specifies the Inbox rule that you want to view.
+        .PARAMETER Encoding
+            Encoding is the parameter specifying the encoding of the CSV output file.
+            Default: UTF8
 
-	.PARAMETER OutputDir
-	OutputDir is the parameter specifying the output directory.
-	Default: Output\Rules
+        .EXAMPLE
+            Get-mailboxRules -UserIds Test@Invictus-ir.com
+            Get-mailboxRules -UserIds "HR@invictus-ir.com,Test@Invictus-ir.com"
+    #>
 
-	.PARAMETER Encoding
-	Encoding is the parameter specifying the encoding of the CSV output file.
-	Default: UTF8
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$UserIds,
 
-    .Example
-	Get-mailboxRules -UserIds Test@Invictus-ir.com
-    Get-mailboxRules -UserIds "HR@invictus-ir.com,Test@Invictus-ir.com"
-#>
-	[CmdletBinding()]
-	param(
-		[string]$UserIds,
-		[string]$OutputDir,
-		[string]$Encoding
-	)
+        [string]$OutputDir = "Output\Rules",
+        [string]$Encoding = "UTF8"
+    )
 
-	$RuleList = @()
+    # Validates the output directory
+    if (!(Test-Path -Path $OutputDir)) {
+        # Creates the output directory if it doesn't exist
+        New-Item -ItemType Directory -Force -Path $OutputDir | Out-Null
+        Write-LogFile -Message "[INFO] Creating the following directory: $OutputDir"
+    }
 
-	if ($Encoding -eq "") {
-		$Encoding = "UTF8"
-	}
+    $RuleList = @()
 
-	if ($OutputDir -eq "") {
-		$OutputDir = "Output\Rules"
-		if (!(test-path $OutputDir)) {
-			write-LogFile -Message "[INFO] Creating the following directory: $OutputDir"
-			New-Item -ItemType Directory -force -Name $OutputDir | Out-Null
-		}
-	}
+    if ($Encoding -eq "") {
+        $Encoding = "UTF8"
+    }
 
-	else{
-		if (Test-Path -Path $OutputDir) {
-			write-LogFile -Message "[INFO] Custom directory set to: $OutputDir"
-		}
+    # Splits the UserIds parameter into an array of user IDs
+    $userIdsArray = $UserIds -split ","
 
-		else {
-			write-LogFile -Message "[Error] Custom directory invalid: $OutputDir exiting script"
-			write-Error "[Error] Custom directory invalid: $OutputDir exiting script" -ErrorAction Stop
-		}
-	}
+    # Loops through each user ID
+    foreach ($userId in $userIdsArray) {
+        # Trims whitespace from the user ID
+        $userId = $userId.Trim()
 
-	$filename = "$($date)-MailboxRules.csv"
-	$outputDirectory = Join-Path $OutputDir $filename
+        # Gets the user object from Microsoft Graph API
+        $user = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$userId" -OutputType Microsoft.Graph.User
 
-	$amountofRules = 0
-	if ($UserIds -eq "") {
-		$totalRules = 0
-		Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users?$select=id,mail" | ForEach-Object {
-			$user = $_
-			$inboxrule = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$($user.id)/mailboxRules" -OutputType Microsoft.Open.MSG.Clients.MailboxRules.MicrosoftGraph.TrustPolicy
-			if ($inboxrule) {
-				$amountofRules = 0
-				foreach ($rule in $inboxrule.Value) {
-					$tempval = [pscustomobject]@{
-						UserName = $user.mail
-						RuleName = $rule.name
-						RuleEnabled = $rule.IsEnabled
-						CopytoFolder = $rule.CopyToFolder
-						MovetoFolder = $rule.MoveToFolder
-						RedirectTo = $rule.RedirectTo
-						ForwardTo = $rule.ForwardTo
-						TextDescription = $rule.Description
-					}
+        if ($null -ne $user) {
+            # Gets the mailbox rules for the user
+            $inboxrule = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$($user.id)/mailboxRules" -OutputType Microsoft.Open.MSG.Clients.MailboxRules.MicrosoftGraph.TrustPolicy
 
-					$RuleList = $tempval
-					$amountofRules = $amountofRules + 1
-					$totalRules = $totalRules + 1
-					$RuleList | export-CSV $outputDirectory -Append -NoTypeInformation -Encoding UTF8
-				}
+            if ($inboxrule) {
+                # Loops through each mailbox rule
+                foreach ($rule in $inboxrule.Value) {
+                    $RuleList += [PSCustomObject]@{
+                        UserName       = $user.Mail
+                        RuleName       = $rule.Name
+                        RuleEnabled    = $rule.IsEnabled
+                        CopytoFolder   = $rule.CopyToFolder
+                        MovetoFolder   = $rule.MoveToFolder
+                        RedirectTo    = $rule.RedirectTo
+                        ForwardTo     = $rule.ForwardTo
+                        TextDescription = $rule.Description
+                    }
+                }
 
-				write-LogFile -Message "[INFO] Found $amountofRules InboxRule(s) for: $($user.mail)..." -Color "Yellow"
-				write-LogFile -Message "[INFO] Collecting $amountofRules InboxRule(s) for: $($user.mail)..." -Color "Yellow"
-			}
-		}
+                # Displays a message indicating that the mailbox rules for the user are collected
+                Write-LogFile -Message "[INFO] Found $($inboxrule.Value.Count) MailboxRule(s) for: $($user.Mail)..." -Color "Yellow"
+                Write-LogFile -Message "[INFO] Collecting $($inboxrule.Value.Count) MailboxRule(s) for: $($user.Mail)..." -Color "Yellow"
+            }
+            else {
+                # Displays a message indicating that no mailbox rules are found for the user
+                Write-LogFile -Message "[INFO] No MailboxRules found for: $($user.Mail)" -Color "Yellow"
+            }
+        }
+        else {
+            # Displays an error message if the user is not found
+            Write-LogFile -Message "[ERROR] User not found: $userId" -Color "Red"
+        }
+    }
 
-	else {
-		if ($UserIds -match ",") {
-			$UserIds.Split(",") | ForEach-Object {
-				$user = $_
+    $filename = "$($date)-MailboxRules.csv"
+    $outputDirectory = Join-Path $OutputDir $filename
 
-				Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$user" | ForEach-Object {
-					$inboxrule = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$($user.id)/mailboxRules" -OutputType Microsoft.Open.MSG.Clients.MailboxRules.MicrosoftGraph.TrustPolicy
-					if ($inboxrule) {
-						$amountofRules = 0
-						foreach ($rule in $inboxrule.Value) {
-							$tempval = [pscustomobject]@{
-								UserName = $user
-								RuleName = $rule.name
-								RuleEnabled = $rule.IsEnabled
-								CopytoFolder = $rule.CopyToFolder
-								MovetoFolder = $rule.MoveToFolder
-								RedirectTo = $rule.RedirectTo
-								ForwardTo = $rule.ForwardTo
-								TextDescription = $rule.Description
-							}
+    # Exports the mailbox rules to a CSV file
+    $RuleList | Export-Csv -Path $outputDirectory -Encoding $Encoding -NoTypeInformation
 
-							$RuleList = $tempval
-							$amountofRules = $amountofRules + 1
-							$totalRules = $totalRules + 1
-							$RuleList | export-CSV $outputDirectory -Append -NoTypeInformation -Encoding UTF8
-						}
-
-						write-LogFile -Message "[INFO] Found $amountofRules InboxRule(s) for: $user..." -Color "Yellow"
-						write-LogFile -Message "[INFO] Collecting $amountofRules InboxRule(s) for: $user..." -Color "Yellow"
-					}
-				}
-			}
-		}
-
-		write-LogFile -Message "[INFO] A total of $totalRules InboxRules found!" -Color "Green"
-		write-LogFile -Message "[INFO] InboxRules rules are collected and writen to: $outputDirectory" -Color "Green"
-	}
+    # Displays a success message
+    Write-LogFile -Message "[INFO] MailboxRules rules are collected and writen to: $outputDirectory" -Color "Green"
 }
