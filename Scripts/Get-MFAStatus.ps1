1ebc3c6259d8def4877a0cc3b743e2b2b59a02ea
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 23:01:57 +0100

    Reapply "improve"
    This reverts commit 4e77377fe35e8684363f5892bb793fa022ba0739.

--- Scripts/Get-MFAStatus.ps1
+++ Scripts/Get-MFAStatus.ps1
@@ -1,5 +1,58 @@
 using module  "$PSScriptRoot\Microsoft-Extractor-Suite.psm1";
 
+function Write-LogFile([string]$Message, [string]$Color = "White") {
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $messageColor = @{
+        Red     = "DarkRed"
+        Green   = "Green"
+        Yellow  = "Yellow"
+        White   = "White"
+    }
+
+    $color = $messageColor[$Color]
+
+    $logMessage = "$timestamp [$Color] $Message"
+    Write-Host $logMessage -ForegroundColor $color
+    Add-Content -Path "$PSScriptRoot\log.txt" -Value $logMessage
+}
+
+function Connect-MgGraph([string[]]$Scopes, [switch]$NoWelcome) {
+    $moduleName = "Microsoft.Graph"
+    $moduleVersion = "1.0.0"
+
+    if (-not (Get-Module -Name $moduleName -ListAvailable)) {
+        Write-LogFile -Message "Installing the Microsoft.Graph module..." -Color Yellow
+        Install-Module -Name $moduleName -Force -AllowClobber -Scope CurrentUser
+    }
+
+    if (-not (Get-Module -Name $moduleName -ListAvailable)) {
+        Write-LogFile -Message "Failed to install the Microsoft.Graph module. Please install it manually and try again." -Color Red
+        return
+    }
+
+    if (-not (Get-Module -Name $moduleName -Name $moduleVersion)) {
+        Write-LogFile -Message "The required version of the Microsoft.Graph module is not installed. Installing it now..." -Color Yellow
+        Uninstall-Module -Name $moduleName -Force -ErrorAction SilentlyContinue
+        Install-Module -Name $moduleName -Force -AllowClobber -Scope CurrentUser -RequiredVersion $moduleVersion
+    }
+
+    if (-not (Get-Module -Name $moduleName -Name $moduleVersion)) {
+        Write-LogFile -Message "Failed to install the required version of the Microsoft.Graph module. Please install it manually and try again." -Color Red
+        return
+    }
+
+    $module = Import-Module -Name $moduleName -PassThru -MinimumVersion $moduleVersion
+
+    if (-not $module.ExportedCommands.ContainsKey("Connect-MgGraph")) {
+        Write-LogFile -Message "The required Connect-MgGraph cmdlet is not found in the Microsoft.Graph module. Please install the latest version and try again." -Color Red
+        return
+    }
+
+    if (-not (Get-MgSession)) {
+        Connect-MgGraph -Scopes $Scopes -NoWelcome
+    }
+}
+
 function Get-MFA {
     [CmdletBinding()]
     param(
@@ -13,72 +66,93 @@ function Get-MFA {
     }
 
     $scopes = @('UserAuthenticationMethod.Read.All', 'User.Read.All')
-    if (-not $Application.IsPresent) {
+
+    if (-not (Get-MgSession)) {
         Connect-MgGraph -Scopes $scopes -NoWelcome
+    }
+
+    if (-not $Application.IsPresent) {
+        try {
+            $session = Get-MgSession
+            if (-not $session.AccessToken) {
+                Connect-MgGraph -Scopes $scopes -NoWelcome
+            }
+        }
+        catch {
+            Write-LogFile -Message "You must call Connect-MgGraph -Scopes '$($scopes -join ',')' before running this script" -Color "Red"
+            return
+        }
     }
 
     try {
         $users = Get-MgUser -All
     }
     catch {
-        Write-LogFile -Message "You must call Connect-MgGraph -Scopes '$($scopes -join ',')' before running this script" -Color "Red"
+        Write-LogFile -Message "Error while retrieving users: $_" -Color "Red"
         return
     }
 
+    $mfaStatus = "Disabled"
+    $mfaMethods = @{}
+
+    foreach ($user in $users) {
+        try {
+            $mfaData = Get-MgUserAuthenticationMethod -UserId $user.UserPrincipalName
+
+            if ($mfaData) {
+                $mfaStatus = "Enabled"
+                break
+            }
+        }
+        catch {
+            Write-LogFile -Message "Error while retrieving the MFA status for $($user.UserPrincipalName): $_" -Color "Red"
+        }
+    }
+
     $results = foreach ($user in $users) {
-        $mfaStatus = "Disabled"
         $mfaMethods = @{}
 
         try {
             $mfaData = Get-MgUserAuthenticationMethod -UserId $user.UserPrincipalName
 
-            foreach ($method in $mfaData) {
-                switch ($method.AdditionalProperties.["@odata.type"]) {
-                    "#microsoft.graph.emailAuthenticationMethod" {
-                        $mfaMethods.email = $true
-                        $mfaStatus = "Enabled"
-                    }
-
-                    "#microsoft.graph.fido2AuthenticationMethod" {
-                        $mfaMethods.fido2 = $true
-                        $mfaStatus = "Enabled"
-                    }
-
-                    "#microsoft.graph.microsoftAuthenticatorAuthenticationMethod" {
-                        $mfaMethods.app = $true
-                        $mfaStatus = "Enabled"
-                    }
-
-                    "#microsoft.graph.passwordAuthenticationMethod" {
-                        $mfaMethods.password = $true
-                        if ($mfaStatus -ne "Enabled") {
-                            $mfaStatus = "Disabled"
+            if ($mfaData) {
+                foreach ($method in $mfaData) {
+                    switch ($method.AdditionalProperties.["@odata.type"]) {
+                        "#microsoft.graph.emailAuthenticationMethod" {
+                            $mfaMethods.email = $true
                         }
-                    }
 
-                    "#microsoft.graph.phoneAuthenticationMethod" {
-                        $mfaMethods.phone = $true
-                        $mfaStatus = "Enabled"
-                    }
+                        "#microsoft.graph.fido2AuthenticationMethod" {
+                            $mfaMethods.fido2 = $true
+                        }
 
-                    "#microsoft.graph.softwareOathAuthenticationMethod" {
-                        $mfaMethods.softwareoath = $true
-                        $mfaStatus = "Enabled"
-                    }
+                        "#microsoft.graph.microsoftAuthenticatorAuthenticationMethod" {
+                            $mfaMethods.app = $true
+                        }
 
-                    "#microsoft.graph.windowsHelloForBusinessAuthenticationMethod" {
-                        $mfaMethods.hellobusiness = $true
-                        $mfaStatus = "Enabled"
-                    }
+                        "#microsoft.graph.passwordAuthenticationMethod" {
+                            $mfaMethods.password = $true
+                        }
 
-                    "#microsoft.graph.temporaryAccessPassAuthenticationMethod" {
-                        $mfaMethods.temporaryAccessPassAuthenticationMethod = $true
-                        $mfaStatus = "Enabled"
-                    }
+                        "#microsoft.graph.phoneAuthenticationMethod" {
+                            $mfaMethods.phone = $true
+                        }
 
-                    "#microsoft.graph.certificateBasedAuthConfiguration" {
-                        $mfaMethods.certificateBasedAuthConfiguration = $true
-                        $mfaStatus = "Enabled"
+                        "#microsoft.graph.softwareOathAuthenticationMethod" {
+                            $mfaMethods.softwareoath = $true
+                        }
+
+                        "#microsoft.graph.windowsHelloForBusinessAuthenticationMethod" {
+                            $mfaMethods.hellobusiness = $true
+                        }
+
+                        "#microsoft.graph.temporaryAccessPassAuthenticationMethod" {
+                            $mfaMethods.temporaryAccessPassAuthenticationMethod = $true
+                        }
+
+                        "#microsoft.graph.certificateBasedAuthConfiguration" {
+                            $mfaMethods.certificateBasedAuthConfiguration = $true
+                        }
                     }
                 }
             }
