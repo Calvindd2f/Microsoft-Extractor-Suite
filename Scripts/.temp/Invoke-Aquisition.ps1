eb5bfb94ea905a987285187968cdd95c29b6a514
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 22:59:40 +0100

    Revert "improve"
    This reverts commit 924eb27773d30a637f13c183b293d09a88ab07d9.

--- Scripts/.temp/Invoke-Aquisition.ps1
+++ Scripts/.temp/Invoke-Aquisition.ps1
@@ -1,36 +1,24 @@
 using namespace System.Net
-using namespace System.Collections.Generic
-
-# Dot sourcing the required scripts
-. "$PSScriptRoot\Microsoft-Extractor-Suite.psm1"
+. "$PSScriptRoot\Microsoft-Extractor-Suite.psm1";
 . "$PSScriptRoot\Scripts\*.ps1"
 
-function Invoke-Aquisition {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory=$true)]
-        [string]$user,
 
-        [Parameter(Mandatory=$true)]
-        [string]$OutputDir,
-
-        [Parameter(Mandatory=$true)]
-        [string]$Encoding
-    )
-
-    $jobs = @()
-
-    # Creating a custom object
-    $response = [PSCustomObject]@{
-        Body = "The name passed was [$($global:Name)] with value of [$($global:Value)]"
-    }
-
-    # Setting the query parameters as global variables
-    $QueryParams = $Request.Query.GetEnumerator()
-    foreach ($param in $QueryParams) {
-        New-Variable -Name $param.Key -Value $param.Value -Force -Scope Global
-    }
+function Invoke-Aquisition([string]$user, [string]$OutputDir, [string]$Encoding)
+{
+	$jobs=@();
 }
 
-# Calling the function
-Invoke-Aquisition -user "testUser" -OutputDir "C:\temp" -Encoding "UTF8"
+
+
+[pscustomobject]@{
+	Name = 'thing1'
+	Value = 'value1'
+}
+
+$Request.Query.GetEnumerator() | ForEach-Object {
+	New-Variable -Name $_.Key -Value $_.Value
+}
+
+$response = @{
+	Body = "The name passed was [$Name] with value of [$Value]
+}
