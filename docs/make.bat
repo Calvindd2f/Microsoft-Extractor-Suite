b3fd98cb3bfea8c0f8ef33c6e6f3ea662e3fb968
Calvindd2f <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 08:42:10 +0100

    improve

--- docs/make.bat
+++ docs/make.bat
@@ -1,35 +1,48 @@
-@ECHO OFF
+@echo off
 
+rem Set the path to the current directory
 pushd %~dp0
 
-REM Command file for Sphinx documentation
+rem Command file for Sphinx documentation
 
-if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
+rem Check if SPHINXBUILD is set, if not set it to sphinx-build
+if not defined SPHINXBUILD (
+    set "SPHINXBUILD=sphinx-build"
 )
-set SOURCEDIR=source
-set BUILDDIR=build
 
-if "%1" == "" goto help
+rem Set the source and build directories
+set "SOURCEDIR=source"
+set "BUILDDIR=build"
 
-%SPHINXBUILD% >NUL 2>NUL
+rem Check if a command was provided as an argument
+if "%~1" == "" (
+    goto help
+)
+
+rem Check if sphinx-build is installed
+%SPHINXBUILD% >nul 2>nul
 if errorlevel 9009 (
-	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
-	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.http://sphinx-doc.org/
-	exit /b 1
+    echo.
+    echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+    echo.installed, then set the SPHINXBUILD environment variable to point
+    echo.to the full path of the 'sphinx-build' executable. Alternatively you
+    echo.may add the Sphinx directory to PATH.
+    echo.
+    echo.If you don't have Sphinx installed, grab it from
+    echo.http://sphinx-doc.org/
+    exit /b 1
 )
 
+rem Build the documentation
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+
+rem Go to the end label
 goto end
 
 :help
+rem Show the help message
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
 :end
+rem Go back to the previous directory
 popd
