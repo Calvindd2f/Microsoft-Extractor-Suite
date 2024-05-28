abea5c18f2dbbf68d5580b1a294012743cf8f7d6
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 22:58:45 +0100

    Revert "improve"
    This reverts commit 515cd863bd4cab8ba1d859cd849add3003247662.

--- Scripts/.temp/binary/ignore/Dependencies.ps1
+++ Scripts/.temp/binary/ignore/Dependencies.ps1
@@ -1,47 +1,41 @@
 #Requires -Version 5.1
-
 <#
-.SYNOPSIS
-    This script verifies the required PowerShell modules used by the assessment tool are installed.
-.PARAMETER Force
+    .SYNOPSIS
+        This script verifies the required Powershell modules used by the
+        assessment tool are installed.
+    .PARAMETER Force
     This will cause all required dependencies to be installed and updated to latest.
-.DESCRIPTION
-    Verifies a supported version of the modules required to support SCuBAGear are installed.
+    .DESCRIPTION
+        Verifies a supported version of the modules required to support SCuBAGear are installed.
 #>
 
 $RequiredModulesPath = Join-Path -Path $PSScriptRoot -ChildPath "RequiredVersions.ps1"
-
-if (-not (Test-Path -Path $RequiredModulesPath)) {
+if (Test-Path -Path $RequiredModulesPath){
+    . $RequiredModulesPath
+}
+else {
     Write-Error "Unable to find required modules path at $RequiredModulesPath"
-    return
 }
 
-. $RequiredModulesPath
-
-if (-not $ModuleList -or $ModuleList.Count -eq 0) {
-    throw "Required modules list is required."
+if (!$ModuleList){
+   throw "Required modules list is required."
 }
 
-$SupportModulesPath = Join-Path -Path $PSScriptRoot -ChildPath "Modules\Support\Support.psm1"
-Import-Module -Name $SupportModulesPath -Force
+$SupportModulesPath = Join-Path -Path $PSScriptRoot -ChildPath "Modules/Support/Support.psm1"
+Import-Module -Name $SupportModulesPath
 
 $MissingModules = @()
 
 foreach ($Module in $ModuleList) {
     Write-Debug "Evaluating module: $($Module.ModuleName)"
     $InstalledModuleVersions = Get-Module -ListAvailable -Name $($Module.ModuleName)
-
-    if ($InstalledModuleVersions -eq $null -or $InstalledModuleVersions.Count -eq 0) {
-        $MissingModules += $Module
-        continue
-    }
-
     $FoundAcceptableVersion = $false
 
-    foreach ($ModuleVersion in $InstalledModuleVersions) {
-        if (($ModuleVersion.Version -ge $Module.ModuleVersion) -and ($ModuleVersion.Version -le $Module.MaximumVersion)) {
+    foreach ($ModuleVersion in $InstalledModuleVersions){
+
+        if (($ModuleVersion.Version -ge $Module.ModuleVersion) -and ($ModuleVersion.Version -le $Module.MaximumVersion)){
             $FoundAcceptableVersion = $true
-            break
+            break;
         }
     }
 
@@ -50,7 +44,7 @@ foreach ($Module in $ModuleList) {
     }
 }
 
-if ($MissingModules.Count -gt 0) {
+if ($MissingModules.Count -gt 0){
     # Set preferences for writing messages
     $PreferenceStack = New-Object -TypeName System.Collections.Stack
     $PreferenceStack.Push($WarningPreference)
@@ -61,10 +55,9 @@ if ($MissingModules.Count -gt 0) {
     Run Initialize-SCuBA to install all required dependencies.
     See Get-Help Initialize-SCuBA for more help."
 
-    Write-Debug "The following modules are not installed or have unsupported versions:"
-
-    foreach ($Module in $MissingModules) {
-        Write-Debug "`t$($Module.ModuleName) (required version: $($Module.ModuleVersion) to $($Module.MaximumVersion)))"
+    Write-Debug "The following modules are not installed:"
+    foreach ($Module in $MissingModules){
+        Write-Debug "`t$($Module.ModuleName)"
     }
 
     $WarningPreference = $PreferenceStack.Pop()
