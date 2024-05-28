14df8c4d2349b96ae603bc0da7f07e4f102c4ef0
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-05-28 23:01:08 +0100

    Revert "improve"
    This reverts commit b18c185f49f412548939dcbabce2c9cba28c9dcb.

--- Scripts/Get-Rules.ps1
+++ Scripts/Get-Rules.ps1
@@ -20,7 +20,7 @@ function Show-TransportRules {
     #>
 
     # Gets the transport rules from Microsoft Graph API
-    $transportRules = Get-MgTransportRules
+    $transportRules = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/policies/transportRules" -OutputType Microsoft.Open.MSG.Clients.TransportRules.MicrosoftGraph.TrustPolicy
 
     if ($null -ne $transportRules) {
         # Displays information message
@@ -79,7 +79,7 @@ function Get-TransportRules {
     $outputDirectory = Join-Path $OutputDir $filename
 
     # Gets the transport rules from Microsoft Graph API
-    $transportRules = Get-MgTransportRules
+    $transportRules = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/policies/transportRules" -OutputType Microsoft.Open.MSG.Clients.TransportRules.MicrosoftGraph.TrustPolicy
 
     if ($null -ne $transportRules) {
         # Creates a StreamWriter to write to the CSV file
@@ -105,85 +105,7 @@ function Get-TransportRules {
     }
 }
 
-# Gets the mailbox rules for the current user and saves them to a CSV file
-function Get-CurrentUserMailboxRules {
-    <#
-        .SYNOPSIS
-            Collects all the mailbox rules for the current user and saves them to a CSV file.
-
-        .DESCRIPTION
-            Collects all the mailbox rules for the current user and saves them to a CSV file.
-
-        .EXAMPLE
-            Get-CurrentUserMailboxRules
-    #>
-
-    [CmdletBinding()]
-    param ()
-
-    # Gets the mailbox rules for the current user
-    $mailboxRules = Get-MgCurrentUserMailboxRules
-
-    if ($null -ne $mailboxRules) {
-        # Creates a StreamWriter to write to the CSV file
-        $streamWriter = [IO.StreamWriter]::Create("Output\Rules\$($date)-TransportRules.csv", $false, [Text.Utf8Encoding]::new("UTF8", $false, $true))
-
-        try {
-            # Writes the header row to the CSV file
-            $streamWriter.WriteLine("Name,Description,CreatedBy,WhenChanged,State")
-
-            # Loops through each mailbox rule
-            $mailboxRules.Value | ForEach-Object {
-                # Writes the properties of the mailbox rule to the CSV file
-                $streamWriter.WriteLine("$($_.DisplayName),$($_.Description),$($_.CreatedBy.Application.DisplayName),$($_.CreatedBy.DateTime),$($_.IsEnabled)")
-            }
-        }
-        finally {
-            # Closes the StreamWriter
-            $streamWriter.Dispose()
-        }
-
-        # Displays a success message
-        Write-LogFile -Message "[INFO] Mailbox rules are collected and writen to: Output\Rules\$($date)-TransportRules.csv" -Color "Green"
-    }
-}
-
-# Displays the mailbox rules for the current user
-function Show-CurrentUserMailboxRules {
-    <#
-        .SYNOPSIS
-            Displays the mailbox rules for the current user.
-
-        .DESCRIPTION
-            Displays the mailbox rules for the current user.
-
-        .EXAMPLE
-            Show-CurrentUserMailboxRules
-    #>
-
-    # Gets the mailbox rules for the current user
-    $mailboxRules = Get-MgCurrentUserMailboxRules
-
-    if ($null -ne $mailboxRules) {
-        # Displays information message
-        Write-LogFile -Message "[INFO] Checking all MailboxRules"
-
-        # Loops through each mailbox rule
-        $mailboxRules.Value | ForEach-Object {
-            # Displays a message indicating that a mailbox rule is found
-            [void](Write-LogFile -Message "[INFO] Found a MailboxRule" -Color "Green")
-
-            # Displays the name, created by, when changed, state, and description of the mailbox rule
-            [void](Write-LogFile -Message "Rule Name: $($_.DisplayName)" -Color "Yellow")
-            [void](Write-LogFile -Message "Rule CreatedBy: $($_.CreatedBy.Application.DisplayName)" -Color "Yellow")
-            [void](Write-LogFile -Message "When Changed: $($_.CreatedBy.DateTime)" -Color "Yellow")
-            [void](Write-LogFile -Message "Rule State: $($_.IsEnabled)" -Color "Yellow")
-            [void](Write-LogFile -Message "Description: $($_.Description)" -Color "Yellow")
-        }
-    }
-}
-
-# Gets the mailbox rules for the specified users and saves them to a CSV file
+# Collects the mailbox rules for the specified users and saves them to a CSV file
 function Get-MailboxRules {
     <#
         .SYNOPSIS
@@ -226,6 +148,10 @@ function Get-MailboxRules {
 
     $RuleList = @()
 
+    if ($Encoding -eq "") {
+        $Encoding = "UTF8"
+    }
+
     # Splits the UserIds parameter into an array of user IDs
     $userIdsArray = $UserIds -split ","
 
@@ -235,11 +161,11 @@ function Get-MailboxRules {
         $userId = $userId.Trim()
 
         # Gets the user object from Microsoft Graph API
-        $user = Get-MgUser -UserId $userId -ErrorAction Stop
+        $user = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$userId" -OutputType Microsoft.Graph.User
 
         if ($null -ne $user) {
             # Gets the mailbox rules for the user
-            $inboxrule = Get-MgUserMailboxRules -UserId $user.Id -ErrorAction Stop
+            $inboxrule = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$($user.id)/mailboxRules" -OutputType Microsoft.Open.MSG.Clients.MailboxRules.MicrosoftGraph.TrustPolicy
 
             if ($inboxrule) {
                 # Loops through each mailbox rule
@@ -280,67 +206,3 @@ function Get-MailboxRules {
     # Displays a success message
     Write-LogFile -Message "[INFO] MailboxRules rules are collected and writen to: $outputDirectory" -Color "Green"
 }
-
-# Gets the current user's mailbox rules
-function Get-MgCurrentUserMailboxRules {
-    $uri = "https://graph.microsoft.com/v1.0/me/mailboxRules"
-    Invoke-MgGraphRequest -Method GET -Uri $uri
-}
-
-# Gets the transport rules from Microsoft Graph
-function Get-MgTransportRules {
-    $uri = "https://graph.microsoft.com/v1.0/policies/transportRules"
-    Invoke-MgGraphRequest -Method GET -Uri $uri
-}
-
-# Gets the user object from Microsoft Graph
-function Get-MgUser([string]$UserId, [switch]$ThrowIfNotFound) {
-    $uri = "https://graph.microsoft.com/v1.0/users/$UserId"
-    Invoke-MgGraphRequest -Method GET -Uri $uri -ErrorAction SilentlyContinue
-
-    if ($ThrowIfNotFound) {
-        if ($null -eq $_) {
-            throw "User not found: $UserId"
-        }
-    }
-}
-
-# Gets the mailbox rules for the user
-function Get-MgUserMailboxRules([string]$UserId, [switch]$ThrowIfNotFound) {
-    $uri = "https://graph.microsoft.com/v1.0/users/$UserId/mailboxRules"
-    Invoke-MgGraphRequest -Method GET -Uri $uri -ErrorAction SilentlyContinue
-
-    if ($ThrowIfNotFound) {
-        if ($null -eq $_) {
-            throw "Mailbox rules not found for user: $UserId"
-        }
-    }
-}
-
-# Invokes a Microsoft Graph API request
-function Invoke-MgGraphRequest([string]$Method, [string]$Uri, [string]$Body, [hashtable]$Headers, [string]$OutputType) {
-    $headers = @{
-        "Content-Type" = "application/json"
-    }
-
-    if ($Headers) {
-        $headers.AddRange($Headers)
-    }
-
-    $response = Invoke-RestMethod -Method $Method -Uri $Uri -Headers $headers -Body $Body
-
-    if ($OutputType) {
-        if ($OutputType -eq "Microsoft.Graph.User") {
-            return [Microsoft.Graph.User]::new()
-        }
-        elseif ($OutputType -eq "Microsoft.Graph.TransportRule") {
-            return [Microsoft.Graph.TransportRule]::new()
-        }
-        elseif ($OutputType -eq "Microsoft.Open.MSG.Clients.MailboxRules.MicrosoftGraph.TrustPolicy") {
-            return [Microsoft.Open.MSG.Clients.MailboxRules.MicrosoftGraph.TrustPolicy]::new()
-        }
-    }
-
-    return $response
-}
-
