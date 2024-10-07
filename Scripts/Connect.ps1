fa6405c28b47bfb40440307f6d97b7394952759e
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-10-07 23:39:59 +0100

    feat: ♿️ Allow `Connect` cmdlets to have parameters 'passed-thru' natively.
    Allows the connect.ps1 cmdlets to have parameters of the original function to connect to service passed through by declaring the originating cmdlet parameters passed through via `@PSBoundParameters` . Can be null/contain no passed parameters. This is an accessibility change.
    
    No breaking changes nor issues closed with this change.

--- Scripts/Connect.ps1
+++ Scripts/Connect.ps1
@@ -1,18 +1,178 @@
 Function Connect-M365
 {
+	PARAM(
+		[string]
+		$ConnectionUri,
+		[string]
+		$AzureADAuthorizationEndpointUri,
+		[ValidateSet('O365China', 'O365Default', 'O365GermanyCloud', 'O365USGovDoD', 'O365USGovGCCHigh')]
+		[ExchangeEnvironment]
+		$ExchangeEnvironmentName,
+		[PSSessionOptions]
+		$PSSessionOptions,
+		[string]
+		$DelegatedOrganization,
+		[string]
+		$Prefix,
+		[string[]]
+		$CommandName,
+		[string[]]
+		$FormatTypeName,
+		[string]
+		$AccessToken,
+		[string]
+		$AppId,
+		[switch]
+		$BypassMailboxAnchoring,
+		[X509Certificate2]
+		$Certificate,
+		[string]
+		$CertificateFilePath,
+		[SecureString]
+		$CertificatePassword,
+		[string]
+		$CertificateThumbprint,
+		[PSCredential]
+		$Credential,
+		[switch]
+		$Device,
+		[switch]
+		$EnableErrorReporting,
+		[switch]
+		$InlineCredential,
+		[string]
+		$LogDirectoryPath,
+		[string]
+		$LogLevel,
+		[switch]
+		$ManagedIdentity,
+		[string]
+		$ManagedIdentityAccountId,
+		[string]
+		$Organization,
+		[uin32]
+		$PageSize,
+		[switch]
+		$ShowBanner,
+		[X509Certificate2]
+		$SigningCertificate,
+		[switch]
+		$SkipLoadingCmdletHelp,
+		[switch]
+		$SkipLoadingFormatData,
+		[Boolean]
+		$TrackPerformance,
+		[Boolean]
+		$UseMultithreading,
+		[string]
+		$UserPrincipalName,
+		[Switch]
+		$UseRPSSession
+	)
 	versionCheck
-	Connect-ExchangeOnline > $null
+	Connect-ExchangeOnline @PSBoundParameters > $null;
 }
 
 Function Connect-Azure
 {
+	PARAM(
+		[ValidateSet('AzureChinaCloud', 'AzureCloud', 'AzureGermanyCloud', 'AzurePPE', 'AzureUSGovernment', 'AzureUSGovernment2', 'AzureUSGovernment3')]
+		[AzureEnvironment+EnvironmentName]
+		$AzureEnvironmentName,
+		[string]
+		$TenantId,
+		[pscredential]
+		$Credential,
+		[string]
+		$CertificateThumbprint,
+		[string]
+		$ApplicationId,
+		[string]
+		$AadAccessToken,
+		[string]
+		$MsAccessToken,
+		[string]
+		$AccountId,
+		[ValidateSet('Error', 'Info', 'None', 'Warning')]
+		[LogLevel]
+		$LogLevel,
+		[string]
+		$LogFilePath,
+		[switch]
+		$WhatIf,
+		[switch]
+		$Confirm,
+		[Switch]
+		$Verbose,
+		[switch]
+		$Debug
+	)
 	versionCheck
-	Connect-AzureAD > $null
+	Connect-AzureAD @PSBoundParameters > $null;
 }
 
 Function Connect-AzureAZ
 {
+	PARAM(
+		[String]
+		$AccessToken ,
+		[String]
+		$AccountId ,
+		[String]
+		$ApplicationId ,
+		[String]
+		$AuthScope ,
+		[SecureString]
+		$CertificatePassword,
+		[String]
+		$CertificatePath ,
+		[String]
+		$CertificateThumbprint ,
+		[String]
+		$ContextName ,
+		[PSCredential]
+		$Credential,
+		[IAzureContextContainer]
+		$DefaultProfile ,
+		[String]
+		$Environment ,
+		[String]
+		$FederatedToken ,
+		[switch]
+		$Force ,
+		[String]
+		$GraphAccessToken ,
+		[switch]
+		$Identity,
+		[String]
+		$KeyVaultAccessToken ,
+		[int]
+		$MaxContextPopulation,
+		[String]
+		$MicrosoftGraphAccessToken ,
+		[ValidateSet('CurrentUser', 'Process')]
+		[ContextModificationScope]
+		$Scope,
+		[switch]
+		$SendCertificateChain,
+		[switch]
+		$ServicePrincipal,
+		[switch]
+		$SkipContextPopulation ,
+		[switch]
+		$SkipValidation ,
+		[String]
+		$Subscription ,
+		[String]
+		$Tenant ,
+		[switch]
+		$UseDeviceAuthentication,
+		[switch]
+		$Confirm,
+		[switch]
+		$WhatIf
+	)
 	versionCheck
-	Connect-AzAccount > $null
+	Connect-AzAccount @PSBoundParameters > $null;
 }
 
